9a7b70ef801da1abe2cd752939ff78ea1c0de8d5
Thomas Aubert <thomas.aubert@edu.devinci.fr>
2024-05-16 10:48:32 +0200

    :white_check_mark: Ex. 3 done

--- classes/classes.spec.js
+++ classes/classes.spec.js
@@ -8,13 +8,6 @@ describe("The User class", () => {
   })});
   
 
-
-
-  // Make new describe
-// should create new error message if the user name is less than 5 characters
-// the name must be at least 5 chars long
-// test validateName() with user2 named "tom", "tom@test.com");
-
 describe("The User class", () => {
   it("should create new error message if the user name is less than 5 characters", () => {
     const user2 = new User("tom", "tom@gmail.com");
@@ -22,3 +15,13 @@ describe("The User class", () => {
     expect(user2.errors[0]).toBe("the name must be at least 5 chars long");
   }
 )});
+
+describe("The User class", () => {
+  it("error the name is require", () => {
+    const user2 = new User("", "tom@gmail.com");
+    user2.validateName(user2.name);
+    expect(user2.errors[0]).toBe("the name is required");
+  }
+)});
+
+
